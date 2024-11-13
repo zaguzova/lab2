e4e24ee11f52a53f2aadd4859c81c727cefe5ea6
zaguzova <gwshnk@gmail.com>
2024-11-13 23:19:31 +0300

    Create лаб1.java

---
+++ лаб1.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Чётные числа от 1 до 100);
+        for (int i = 1; i = 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i +  );
+            }
+        }
+
+        System.out.println(nnНечётные числа от 1 до 100);
+        for (int i = 1; i = 100; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i +  );
+            }
+        }
+    }
+}
